78c42067d242bec7afdbebdfefb411ffc62f14ab
seh223 <seh223@lehigh.edu>
2019-11-14 17:59:20 -0500

    Added a function
    Installed Library package

--- R Script Assignment 05.R
+++ R Script Assignment 05.R
@@ -1,5 +1,8 @@
+install.packages("data.table")
 library (data.table)
+header <- read.table("fredgraph.csv", header = TRUE, sep=",", nrow=1)
 DF <- data.frame(fread("fredgraph.csv"))
 str(DF)
 #Data_Frame.data <- data.frame(DF)
 #str(Data_Frame.data)
+
